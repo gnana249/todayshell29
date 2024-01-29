70bf5c4490b29dfbd2a32f767d648b7a35731203
prakash <saignana545@gmail.com>
2024-01-29 15:45:01 +0530

    modified the file

--- checking_age.sh
+++ checking_age.sh
@@ -8,7 +8,12 @@
  if [ $age -lt 13 ];then
 	 echo "your a child"
  elif [ $age -ge 13 ] && [ $age -le 19 ];then
-	 echo "you are a teenagar"
- elif [ $age -ge 20 ] ;then
-	 echo "your are a adult"
+	 echo "you are a teenagar adlut"
+ elif [ $age -ge 20 ] && [ $age -lt 60 ] ;then
+	 echo "your a adult"
+ elif [ $age -ge 60 ];then
+	 echo "you are a oldage"
+ else
+	 echo"invalid input"
+
  fi
